39dbb5578e2c5b85415a93e5d66fc89dd6486853
Kourosh Latifi <kourosh.latifi@aalto.fi>
2019-10-02 09:49:31 +0300

    Magnitude plots with red and blue colormap. Add colorbar to q-value magnitude plots.

--- Plots and analysis/Analyze learning/plot_q_magnitude.m
+++ Plots and analysis/Analyze learning/plot_q_magnitude.m
@@ -11,36 +11,44 @@ x = linspace(0.3,0.7,numGrid);
 y = linspace(0.1,0.5,numGrid);
 [X,Y] = meshgrid(x,y);
 
+colorMap = buildcmap('bwr');
 selected_episodes = [9 49 99 149 199];
-% cmap = {'parula','jet','hot','bone','cool','spring','summer','autumn','gray','copper','pink'};
-% cmap = {'parula','spring','gray','copper'};
-cmap = {'parula'};
 N_episodes = length(selected_episodes);
-N_cmap = length(cmap);
+  
+for i = 1:N_episodes
 
-
-% surf(X,Y,NetMat(:,:,20));
-for j = 1:N_cmap    
-    for i = 1:N_episodes
-%         subplot(length(cmap),length(selected_episodes),(j-1)*N_episodes+i);
-%         subplot(1,length(selected_episodes),i);
-        figure
-        hold on
-        q_value = NetMat(:,:,selected_episodes(i));
-        pcolor(x,y,q_value);    
-        colormap(cmap{j});
-        shading interp
-        % caxis([-0.3 0.3]);
-        ax = gca;
-        ax.XTick = [];
-        ax.YTick = [];
-        axis square
-        axis equal
-        axis([0.3 0.7 0.1 0.5]);
-        set(gca,'Ydir','reverse'); 
-        box on
-        hold off;        
-        export_fig(strcat(tempPath,'q_value_',num2str(selected_episodes(i))),'-r600');
-    end
+    q_value = NetMat(:,:,selected_episodes(i));  
+    figure       
+    hold on        
+    pcolor(x,y,q_value);    
+    colormap(colorMap);
+    shading interp
+    caxis([-2e-3 2e-3]);
+    ax = gca;
+    ax.XTick = [];
+    ax.YTick = [];
+    axis square
+    axis equal
+    axis([0.3 0.7 0.1 0.5]);
+    set(gca,'Ydir','reverse'); 
+    box on
+    hold off;        
+    export_fig(strcat(tempPath,'q_value_',num2str(selected_episodes(i))),'-r600');
 end
+
+% Generate colorbar
+figure
+colorMap = buildcmap('bwr');
+colormap(colorMap);
+hold on
+shading interp
+c = colorbar;
+c.Ticks = [-2 -1 0 1 2];
+c.FontName = 'Times New Roman';
+c.FontSize = 24;
+caxis([-2 2]);
+set(gcf, 'Color', 'None');
+axis off
+export_fig(strcat(tempPath,'colorbar_q_value'),'-r600');
+
 
