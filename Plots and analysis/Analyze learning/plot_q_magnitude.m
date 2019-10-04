600fe74019d4c67aaa4e7c927584281091a6e895
Kourosh Latifi <kourosh.latifi@aalto.fi>
2019-10-04 10:49:34 +0300

    Minor change to get the colorbar correct.

--- Plots and analysis/Analyze learning/plot_q_magnitude.m
+++ Plots and analysis/Analyze learning/plot_q_magnitude.m
@@ -15,29 +15,29 @@ colorMap = buildcmap('bwr');
 selected_episodes = [9 49 99 149 199];
 N_episodes = length(selected_episodes);
   
-for i = 1:N_episodes
-
-    q_value = NetMat(:,:,selected_episodes(i));  
-    figure       
-    hold on        
-    pcolor(x,y,q_value);    
-    colormap(colorMap);
-    shading interp
-    caxis([-2e-3 2e-3]);
-    ax = gca;
-    ax.XTick = [];
-    ax.YTick = [];
-    axis square
-    axis equal
-    axis([0.3 0.7 0.1 0.5]);
-    set(gca,'Ydir','reverse'); 
-    box on
-    hold off;        
-    export_fig(strcat(tempPath,'q_value_',num2str(selected_episodes(i))),'-r600');
-end
+% for i = 1:N_episodes
+% 
+%     q_value = NetMat(:,:,selected_episodes(i));  
+%     figure       
+%     hold on        
+%     pcolor(x,y,q_value);    
+%     colormap(colorMap);
+%     shading interp
+%     caxis([-2e-3 2e-3]);
+%     ax = gca;
+%     ax.XTick = [];
+%     ax.YTick = [];
+%     axis square
+%     axis equal
+%     axis([0.3 0.7 0.1 0.5]);
+%     set(gca,'Ydir','reverse'); 
+%     box on
+%     hold off;        
+%     export_fig(strcat(tempPath,'q_value_',num2str(selected_episodes(i))),'-r600');
+% end
 
 % Generate colorbar
-figure
+f = figure;
 colorMap = buildcmap('bwr');
 colormap(colorMap);
 hold on
@@ -47,8 +47,8 @@ c.Ticks = [-2 -1 0 1 2];
 c.FontName = 'Times New Roman';
 c.FontSize = 24;
 caxis([-2 2]);
+axis off;
 set(gcf, 'Color', 'None');
-axis off
 export_fig(strcat(tempPath,'colorbar_q_value'),'-r600');
 
 
